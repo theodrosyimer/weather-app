7c72dedf6206ad9767a685c010a61ff85aebb1d4
Theodros Yimer <theox01@hotmail.fr>
2023-04-09 15:29:37 +0200

    fix: use `Math.floor` to match apple weather temp

--- src/components/SavedTown.js
+++ src/components/SavedTown.js
@@ -7,14 +7,14 @@ export function SavedTown({ location, current, forecast: { forecastday } }, { cu
               <div class="city">${currentLocation ? 'My Location' : location.name}</div>
               <div class="time">${location.localtime.split(' ')[1]}</div>
             </div>
-            <div class="current-temperature">${Math.round(current.temp_c)}°</div>
+            <div class="current-temperature">${Math.floor(current.temp_c)}°</div>
           </div>
 
           <div class="flex">
             <div class="short-description">${current.condition.text}</div>
             <div class="highest-lowest-temperatures">
-              H: <span class="highest-temp">${Math.round(forecastday[0].day.maxtemp_c)}°</span>
-              L: <span class="lowest-temp">${Math.round(forecastday[0].day.mintemp_c)}°</span>
+              H: <span class="highest-temp">${Math.floor(forecastday[0].day.maxtemp_c)}°</span>
+              L: <span class="lowest-temp">${Math.floor(forecastday[0].day.mintemp_c)}°</span>
             </div>
           </div>
         </div>
